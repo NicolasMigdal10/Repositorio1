6d6faff57bfdaab13a66cae0a9c0abdf90f7d0e1
usuario <usuario@DESKTOP-RARC6VD>
2019-07-29 17:02:05 -0300

    Primera prueba

---
+++ src/ItemStock.java
@@ -0,0 +1,33 @@
+
+import java.util.Date;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author usuario
+ */
+public class ItemStock {
+    private Computadora computadora;
+    private int cantidad;
+    private Date ultimaFechaModificacion;
+    
+    ItemStock(Computadora computadora,int cantidad){
+    this.computadora= computadora;
+    this.cantidad= cantidad;
+    }
+
+    public Computadora getComputadora() {
+        return computadora;
+    }
+
+    public void setComputadora(Computadora computadora) {
+        this.computadora = computadora;
+    }
+    
+    
+}
---
+++ src/Stock.java
@@ -0,0 +1,28 @@
+
+import java.util.ArrayList;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author usuario
+ */
+public class Stock {
+   private ArrayList<ItemStock>items;
+
+    
+    public ArrayList<ItemStock> getItems() {
+        return items;
+    }
+
+    public void setItems(ArrayList<ItemStock> items) {
+        this.items = items;
+    }
+    
+    
+    
+}
